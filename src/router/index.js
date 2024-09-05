1125b20347036313761c5bd90f7aa2f17fef9321
manager225 <manager225@mail.ru>
2024-09-05 12:19:59 +0400

    add publicPath

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/HomeComponent.vue'
 import Convert from '../views/Convert.vue'
+const publicPath = '/currency_converter'
 
 const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/convert', name: 'Convert', component: Convert }
+  { path: `${publicPath}/`, name: 'Home', component: Home },
+  { path: `${publicPath}/convert`, name: 'Convert', component: Convert }
 ]
 
 const router = createRouter({
